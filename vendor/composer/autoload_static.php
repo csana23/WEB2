de1aa0d9f73282a81cf7224b51808f8b01a3d217
Csanaki Richárd <richard.csanaki@gmail.com>
2019-03-18 00:07:47 +0100

    boi

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -407,6 +407,7 @@ class ComposerStaticInit92c59ea8dc50ca5bdc36ec0c6c1bf6de
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Auth\\VerificationController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/VerificationController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
         'App\\Http\\Controllers\\Travels' => __DIR__ . '/../..' . '/app/Http/Controllers/Travels.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
